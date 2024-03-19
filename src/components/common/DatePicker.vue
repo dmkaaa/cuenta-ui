f4d24499ce87c6853bf2513f4e47ce8168402a89
dmkaaa <1486519+dmkaaa@users.noreply.github.com>
2024-03-19 23:27:50 +0200

    Datepicker

--- src/components/common/DatePicker.vue
+++ src/components/common/DatePicker.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
 import AppInput from '@/components/common/AppInput.vue'
 import { chunk } from '@/util/array'
-import { addDays } from '@/util/date'
+import { addDays, addMonths, addYears } from '@/util/date'
 import { computed, ref } from 'vue'
-
-const now = new Date()
+import AppButtonLink from './AppButtonLink.vue'
 
 const visible = ref(false)
-const year = ref(now.getFullYear())
-const month = ref(now.getMonth())
-const grid = computed(() => getGrid(year.value, month.value))
+const date = ref(getFirstDayOfMonth(new Date()))
+const monthName = computed(() => date.value.toLocaleString('en', { month: 'long' }))
+const grid = computed(() => getGrid(date.value))
 
 function show() {
   visible.value = true
 }
 
-function getGrid(year: number, month: number): Date[][] {
-  const start = new Date(year, month, 1)
+function getFirstDayOfMonth(date: Date): Date {
+  return new Date(date.getFullYear(), date.getMonth(), 1)
+}
+
+function getGrid(date: Date): Date[][] {
+  const start = getFirstDayOfMonth(date)
   const dow = start.getDay()
   const daysBefore = dow === 1 ? 7 : dow - 1
   start.setDate(start.getDate() - daysBefore)
@@ -28,13 +31,42 @@ function getGrid(year: number, month: number): Date[][] {
 
   return chunk(dates, 7)
 }
+
+function incMonth(amount: number) {
+  date.value = addMonths(date.value, amount)
+}
+
+function incYear(amount: number) {
+  date.value = addYears(date.value, amount)
+}
 </script>
 
 <template>
   <AppInput @click="show()" />
-  <div v-if="visible">
+  <div
+    v-if="visible"
+    class="absolute border rounded border-slate-600 bg-slate-700 text-slate-200 mt-1"
+  >
+    <div class="flex justify-between">
+      <div class="px-2">
+        <AppButtonLink @click="incYear(-1)">&larr;</AppButtonLink>
+      </div>
+      <div class="font-semibold">{{ date.getFullYear() }}</div>
+      <div class="px-2">
+        <AppButtonLink @click="incYear(1)">&rarr;</AppButtonLink>
+      </div>
+    </div>
+    <div class="flex justify-between">
+      <div class="px-2">
+        <AppButtonLink @click="incMonth(-1)">&larr;</AppButtonLink>
+      </div>
+      <div>{{ monthName }}</div>
+      <div class="px-2">
+        <AppButtonLink @click="incMonth(1)">&rarr;</AppButtonLink>
+      </div>
+    </div>
     <div v-for="row in grid" class="flex justify-around">
-      <div v-for="cell in row" class="p-1">
+      <div v-for="cell in row" class="w-7 text-center py-1">
         {{ cell.getDate() }}
       </div>
     </div>
--- src/util/date.ts
+++ src/util/date.ts
@@ -7,3 +7,15 @@ export function addDays(date: Date, days: number): Date {
   copy.setDate(copy.getDate() + days)
   return copy
 }
+
+export function addMonths(date: Date, months: number): Date {
+  const copy = new Date(date)
+  copy.setMonth(copy.getMonth() + months)
+  return copy
+}
+
+export function addYears(date: Date, years: number): Date {
+  const copy = new Date(date)
+  copy.setFullYear(copy.getFullYear() + years)
+  return copy
+}
